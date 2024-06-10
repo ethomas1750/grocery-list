5915ccaa4735751155d3a9f73693541c417be7a2
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-10 00:16:10 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,17 @@
+import React, { Component } from 'react'
+import "./App.css"
+import Header from './Header'
+import Product from './Product'
 
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Product/>
+      </div>
+    )
+  }
+}
+
+export default App
